8dcdfacb089acd055dc414927a34bda06248a752
Esanchezl <42590200+esanchezlMBT@users.noreply.github.com>
2019-06-26 16:41:44 -0500

    Release 2.4
    Location results

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ bot.dialog('/', [
             
             case Opts.Ubicacion:
                     // session.send("[Ubicación actual](https://mainbitlabs.github.io/)");
-                session.beginDialog('ubicacion');
+                session.beginDialog('location');
             break;
         }
         
@@ -531,6 +531,20 @@ bot.dialog('cancel',
     }
 ).triggerAction(
     {matches: /(cancel|cancelar)/gi}
+);
+bot.dialog('location', [
+    function (session, results) {
+        
+        builder.prompts.text(session, "Comparte tu ubicación actual");
+       
+    },
+    function (session, results) {
+        if (results.response) {
+            console.log("_ >>> Location Results <<< _",results);
+            session.endConversation("Gracias, hemos terminado por ahora.");
+        }
+    }
+]
 );
 bot.dialog('ubicacion', [
     function (session) {
